c06c3429e335c3021e0b3da4d9bc23b707f83a8c
Your Name <you@example.com>
2021-11-25 11:19:15 +0100

    functions2.php

---
+++ functions2.php
@@ -0,0 +1,12 @@
+<?php
+
+function myDummyFunction(int $age){
+    if($age>=18){
+        echo "jsi dospělý";
+}
+    else{
+        echo "nejsi doslpělí ještě si musíš počkat";
+    }
+}
+        echo"<br>";
+?>
